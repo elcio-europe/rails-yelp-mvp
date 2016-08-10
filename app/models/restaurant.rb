7640c53a33e7cbfc53e9f7b71813db378277c0b0
Elcio <elcio@europe.com>
2016-08-10 15:04:31 -0300

    models ok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  TYPE = %w( chinese italian japanese french belgian)
+
+  has_many :reviews, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: {in: TYPE, allow_nill: false }
+
 end
